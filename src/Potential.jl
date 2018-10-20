2f1dec9f005afb766ca420291f0f4ed98cd04b82
Sunghyun Kim <frssp.kim@gmail.com>
2018-10-20 15:11:33 +0100

    rename Phonon -> Potential

---
+++ src/Potential.jl
@@ -0,0 +1,57 @@
+module Potential
+using Brooglie # Atomic unit
+
+amu = 931.4940954E6   # eV / c^2
+ħc = 0.19732697E-6    # eV m
+
+function solve1D_ev_amu(pot_ev_amu; NQ=100, Qi=-10, Qf=10, nev=30, maxiter=100)
+    factor = (1/amu) * (ħc*1E10)^2
+
+    ϵ1, χ1 = solve(x->pot_ev_amu(x*factor^0.5);
+                  N=NQ, a=Qi/factor^0.5, b=Qf/factor^0.5, m=1, nev=nev, maxiter=maxiter)
+    return ϵ1, χ1/factor^0.25
+end
+
+function plot_potentials(pot, ϵ1, χ1; plt=Nothing, scale_factor=2e-2)
+    if plt == Nothing
+       plt = plot()
+    end
+
+    # Initial state
+    plot!(plt, V1.Q, V1.E, lw=4, color="#bd0026", label="")
+    for i = 1:length(ϵ1)
+        plot!(Q, χ1[i]*scale_factor .+ ϵ1[i],
+              fillrange=[χ1[i]*0 .+ ϵ1[i], χ1[i]*scale_factor .+ ϵ1[i]],
+              c="#bd0026", alpha=0.5, label="")    
+    end
+end
+
+
+# Set of potentials
+function step(x, width, depth; x0=0.)
+    x = x .- x0
+    pot_well = (x .>= -width/2.) .& (x .<= width/2.)
+    return pot_well .* -depth
+end
+
+function harmonic(x, ħω)
+    # ev(amu)
+    a = amu / 2. * (ħω/ħc/1E10)^2
+    return a*x.*x
+end
+
+function double(x, ħω1, ħω2)
+    a1 = amu / 2. * (ħω1/ħc/1E10)^2
+    a2 = amu / 2. * (ħω2/ħc/1E10)^2
+    return - a1*x.*x + a2*x.*x.*x.*x
+end
+
+function polyfunc(x, coeffs, poly_order)
+    y = 0 .* x
+    for i = 1:poly_order + 1
+        y += coeffs[i].*x.^(i-1)
+    end
+    return y
+end
+
+end # module
