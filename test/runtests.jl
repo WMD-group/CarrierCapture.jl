57911dbe6cdc8c8f931297e2e8452a13474d3323
frssp <frssp.kim@gmail.com>
2019-04-02 10:13:17 +0100

    fix test

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,7 +11,6 @@ using Test
     dQ = (Qf-Qi)/NQ
     x = range(Qi, stop = Qf, length = NQ)
     ħω = 1 
-    e, v = solve1D_ev_amu(x->harmonic(x, ħω; param = Dict("E0" => 0, "Q0" => 0)), NQ = NQ , Qi = Qi, Qf = Qf, nev = nev)
-    analytic_sol = UnitRange(0, nev) 
+    e, v = solve1D_ev_amu(x->harmonic(x, ħω,; E₀ = 0, Q₀ = 0), NQ = NQ , Qi = Qi, Qf = Qf, nev = nev)
     @test isapprox(e, 0.5 .+  UnitRange(0, nev-1) ; rtol=etol)
 end
