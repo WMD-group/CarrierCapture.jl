c05b567e0a61a4b03a16862247618bbb9d3013c8
Sunghyun Kim <frssp.kim@gmail.com>
2020-01-09 20:20:37 +0000

    Update test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,15 +2,5 @@
 using CarrierCapture
 using Test
 
-@testset "Vibrational Energy" begin
-    etol = 1e-3
-    NQ = 5000
-    Qi = -5
-    Qf = 5
-    nev = 20
-    dQ = (Qf-Qi)/NQ
-    x = range(Qi, stop = Qf, length = NQ)
-    ħω = 1 
-    e, v = solve1D_ev_amu(x->harmonic(x, ħω,; E₀ = 0, Q₀ = 0), NQ = NQ , Qi = Qi, Qf = Qf, nev = nev)
-    @test isapprox(e, 0.5 .+  UnitRange(0, nev-1) ; rtol=etol)
-end
+include("test_tise.jl")
+include("test_potential.jl")
