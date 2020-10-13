26a09965b12f0dbf81f687046c81187a0f1b8c35
Miguel Rivera <miguel.rivera@hotmail.fr>
2020-10-13 11:46:49 +0100

    Add functionality

--- script/get_del_Q.py
+++ script/get_del_Q.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3.7
 # -*- coding=utf-8 -*-
+"""
+Get the mass weighted coordinate difference between two geometries.
+
+The distance can be in a straight line, or projected on to a vector of initial and final geometries.
+
+"""
 
 import sys
 import os
@@ -32,6 +38,7 @@ def read_poscar(i_path, l_get_sorted_symbols=False):
 
 
 def get_init_fin(i_file, f_file, disp_range=None):
+def get_init_fin(args):
     '''
     '''
     # A. Alkauskas, Q. Yan, and C. G. Van de Walle, Physical Review B 90, 27 (2014)
@@ -54,27 +61,52 @@ def get_init_fin(i_file, f_file, disp_range=None):
     # print(delta_Q.sum())
 
 
-def main(i_file, f_file):
-    '''
-    '''
-    get_init_fin(i_file, f_file)
+def main(args):
+    struct_i = read_poscar(args.init)
+    struct_f, sorted_symbols = read_poscar(args.fin, True)
+    delta_R = struct_f.frac_coords - struct_i.frac_coords
+    delta_R = (delta_R + 0.5) % 1 - 0.5
+    lattice = struct_i.lattice.matrix
+    delta_R = np.dot(delta_R, lattice)
 
+    masses = np.array([spc.atomic_mass for spc in struct_i.species])
 
+    # allow for no mass weighting
+    if args.no_weight:
+        delta_Q2 = delta_R ** 2
+    else:
+        delta_Q2 = masses[:,None] * delta_R ** 2
+    delta_Q = np.sqrt(delta_Q2.sum())
+
+    print("Delta Q:", delta_Q)
+
+    if args.med != "unassigned":
+        struct_m = read_poscar(args.med)
+        delta_M = struct_m.frac_coords - struct_i.frac_coords
+        delta_M = (delta_M + 0.5) % 1 - 0.5
+        # project the midpoint on the delta_R vector
+        delta_M_proj = np.dot(delta_R, delta_M)/np.linalg.norm(delta_R)
+        if args.no_weight:
+            delta_M_proj_Q2 = delta_M_proj ** 2
+        else:
+            delta_M_proj_Q2 = masses[:None] * delta_M_proj ** 2
+        delta_M_proj_Q = np.sqrt(delta_M_proj_Q2.sum())
+
+        print("Projected delta Q:", delta_M_proj_Q)
+        print(delta_M_proj_Q/delta_Q)
 
 if __name__ == '__main__':
-    '''
-    '''
     parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
                                      description="This script calculates the atomic mass weighted distance between two structures.")
     parser.add_argument("-i","--init",
                         help="initial input file (POSCAR format) ",default="./POSCAR_i")
     parser.add_argument("-f","--fin",
                         help="final input file (POSCAR format) ",default="./POSCAR_f")
-    
-    args = parser.parse_args()
-    
-    i_file = args.init
-    f_file = args.fin
+    parser.add_argument("-m","--med",nargs="?",default='unassigned',
+            help="optional medium geometry (POSCAR format) to get its the fractional displacement between the initial and final geometries")
+    parser.add_argument("-nw","--no_weight",action="store_true",help="Turn off mass weighting")
 
-    main(i_file, f_file)
+    args = parser.parse_args()
+
+    main(args)
 
