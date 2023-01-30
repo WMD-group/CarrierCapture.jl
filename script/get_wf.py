0cc1560055a22379848e0c7085eba3d6c98f150d
Xinwei <66780582+xw-w@users.noreply.github.com>
2023-01-30 11:48:26 +0000

    Update get_wf.py
    Specified spin-up/down channel available

--- script/get_wf.py
+++ script/get_wf.py
@@ -3,7 +3,7 @@ np.set_printoptions(4)
 
 import argparse
 
-def main(band_indice, defect_ind, dir_init, dir_final, kpoint_indice, spin_indice):
+def main(band_indice, defect_ind, dir_init, dir_final, kpoint_indice, nspin_indice, spin_indice):
     from pawpyseed.core.projector import Wavefunction, Projector # also imports the wavefunction module
     wf_list = []
     overlap_list = []
@@ -20,7 +20,7 @@ def main(band_indice, defect_ind, dir_init, dir_final, kpoint_indice, spin_indic
         result = result[:, int(kpoint_indice)]
         for b in band_indice:
             band_in=str(b)
-        band_i = int(band_in)*int(spin_indice+1)
+        band_i = int(band_in)*int(nspin_indice+1)+spin_indice
         overlap_list.append(result[band_i])
 
     print("\n\n\n")
@@ -32,7 +32,7 @@ def main(band_indice, defect_ind, dir_init, dir_final, kpoint_indice, spin_indic
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Pawpyseed wrapper: calculating overlap between wave functions \
-        e.g. python get_wf.py   -d 288 -b 291  -D DISP_000 -i DISP_-02 DISP_-01 DISP_001 DISP_002 -k 0 -s 1')
+        e.g. python get_wf.py   -d 288 -b 291  -D DISP_000 -i DISP_-02 DISP_-01 DISP_001 DISP_002 -k 0 -S 1 -s 0')
 
     parser.add_argument('-D','--dir_init', help='directory for the initial state', type=str, required=True, default="DISP_000")
     parser.add_argument('-i','--input', help='defect for deformed structures', type=str, nargs='+', required=True)
@@ -40,7 +40,8 @@ if __name__ == '__main__':
     parser.add_argument('-b','--band', help='band index for a initial state (start at 0)', nargs='+', type=int, required=True)
     parser.add_argument('-d','--defect', help='band index for a final state (start at 0)', type=int, required=True)
     parser.add_argument('-k','--kpoint',help='kpoint index (start at 0)', type=int, required=True, default=0)
-    parser.add_argument('-s','--spin',help='spin-unpolarised/polarised (0/1) calculation', type=int, required=True, default=0)
+    parser.add_argument('-S','--spin',help='spin-unpolarised/polarised (0/1) calculation', type=int, required=True, default=0)
+    parser.add_argument('-s','--spin',help='spin-up/down (0/1) channel', type=int, required=True, default=0)
     args = parser.parse_args()
 
 
@@ -49,6 +50,7 @@ if __name__ == '__main__':
     dir_init  = args.dir_init
     dir_final = args.input
     kpoint_indice = args.kpoint
+    nspin_indice = args.nspin
     spin_indice = args.spin
 
-    main(band_indice, defect_ind, dir_init, dir_final, kpoint_indice, spin_indice)
+    main(band_indice, defect_ind, dir_init, dir_final, kpoint_indice, nspin_indice, spin_indice)
